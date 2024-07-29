16d5d3aa0971b5cbca11b718474d2113f1120d7e
alya1007 <alexandra.konjevic@isa.utm.md>
2024-07-29 22:13:29 +0300

    initial setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+	subsets: ["latin"],
+	weight: ["100", "400", "700", "900"],
+});
+
+export const metadata: Metadata = {
+	title: "Writers application",
+	description: "Post your essays and stories",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${roboto.className} overflow-x-hidden bg-light`}>
+				{children}
+			</body>
+		</html>
+	);
+}
